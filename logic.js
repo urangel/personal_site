935bae27c80fc34ea2bea5cf94aa735596c831b6
Ulises Rangel <uli.atx@gmail.com>
2019-06-13 14:02:59 -0500

    card animation on scroll

--- logic.js
+++ logic.js
@@ -13,3 +13,29 @@ function scroll() {
 scrollButton.onclick = () => {
   scroll();
 };
+
+function animate() {
+  let tarjetas, windowHeight;
+  function init() {
+    tarjetas = document.querySelectorAll('.hidden');
+    windowHeight = window.innerHeight;
+    addEventHandlers();
+    checkPosition();
+  }
+  function addEventHandlers() {
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+  }
+  function checkPosition() {
+    for (let i = 0; i < tarjetas.length; i++) {
+      let positionFromTop = tarjetas[i].getBoundingClientRect().top;
+      if (positionFromTop - windowHeight <= 0) {
+        tarjetas[i].className = tarjetas[i].className.replace('hidden','fade-in-element');
+      }
+    }
+  }
+  return {
+    init: init
+  };
+};
+animate().init();
