793f1282f258b88d23098e73ed7e5d18c3ae4e54
Ulises <uli.atx@gmail.com>
2019-06-18 12:34:46 -0500

    organization and commenting

--- logic.js
+++ logic.js
@@ -1,19 +1,19 @@
 let blurb = document.getElementById("blurb");
 let scrollButton = document.getElementById("scroll-button");
 let portfolio = document.getElementById("portfolio");
-
+//MAKES THE BLURB FADE IN AND FOCUS ON WINDOW LOAD
 window.onload = () => {
   blurb.classList.add("appear");
   blurb.style.opacity = "1";
 }
-
+//BOUNCING HEXAGON MADE INTO SCROLL-BUTTON
 function scroll() {
   portfolio.scrollIntoView();
 }
 scrollButton.onclick = () => {
   scroll();
 };
-
+//FUNCTION FOR CARDS TO APPEAR AND RESIZE WHEN SCROLLED TO
 function animate() {
   let tarjetas, windowHeight;
   function init() {
